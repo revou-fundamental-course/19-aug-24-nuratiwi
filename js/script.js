64f4dea549e86d38cb5b771ccda2c69e0e663e9e
nuratiwi <107332121+nuratiwi@users.noreply.github.com>
2024-08-28 21:13:16 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
 // ini javascript
+
+function formValidation() {
+    //cara ambil dom
+    let name = document.getElementById('fname').value;
+    //cara condition
+    if (name == '') {
+        alert('Empty Form');
+    } else {
+        document.getElementById('result').innerHTML = name;
+    }
+    console.log(name);
+}
+
+let indexSlide = 1;
+showBanner(1);
+
+function nextSlide(n) {
+    showBanner(indexSlide += n);
+}
+
+function showBanner(indexBanner) {
+    let listImage = document.getElementsByClassName('banner-img');
+    console.log('indexBanner:' + indexBanner);
+    console.log('listImage:' + listImage.length);
+    if(indexBanner > listImage.length) indexSlide = 1;
+
+    let index = 0;
+    while (index < listImage.length) {
+        listImage[index].style.display = 'none';
+        index++;
+    }
+
+    listImage[indexSlide-1].style.display = 'block';
+    console.log(listImage);
+}
